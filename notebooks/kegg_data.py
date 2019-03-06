d17a3026571e4270890792a14d2666bc6ccafec5
Ellie James <elliej3@uw.edu>
2019-03-05 19:47:42 -0800

    autopep8 run on these

--- notebooks/kegg_data.py
+++ notebooks/kegg_data.py
@@ -2,9 +2,10 @@
 import pandas as pd
 import pubchempy as pc
 
+
 def sid_to_smiles(sid):
     """Takes an SID and prints the associated SMILES string."""
-    
+
     substance = pc.Substance.from_sid(sid)
     cid = substance.standardized_cid
     compound = pc.get_compounds(cid)[0]
@@ -14,14 +15,15 @@ def sid_to_smiles(sid):
 def kegg_df_to_smiles(kegg_df):
     """Takes a pandas dataframe that includes a column of SIDs, gets the isomeric SMILES for each SID, stores them as a list, then adds a SMILES column."""
 
-    res = [] 
-    
+    res = []
+
     for i in range(len(kegg_df)):
-        sid = kegg_df.iloc[i, 1] #CHANGE THIS 1 TO THE PROPER COLUMN NUMBER FOR SID 
+        # CHANGE THIS 1 TO THE PROPER COLUMN NUMBER FOR SID
+        sid = kegg_df.iloc[i, 1]
         result = sid_to_smiles(sid)
         res.append(result)
-        
-    
-    kegg_df.insert(2, column='SMILES', value=res) #Change this 2 to the number where the smiles column should be
-    
+
+    # Change this 2 to the number where the smiles column should be
+    kegg_df.insert(2, column='SMILES', value=res)
+
     return kegg_df
