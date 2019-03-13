bb7f4cebf20797818542dd2f3d39e4adb8d08482
stephen blaskowski <blasks@uw.edu>
2019-03-13 10:57:01 -0700

    edited data_curation_workflow.ipynb and organized datasets

--- code/cpd_info.py
+++ code/cpd_info.py
@@ -1,8 +1,9 @@
 # create a function that return the general information of that compound e.g., number of each atom, MW, unsaturation
 # this will be cpd_info.py
-
+import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from rdkit.Chem.Descriptors import MolWt
 
 def count_C(mol):
     return sum(1 for atom in mol.GetAtoms() if atom.GetAtomicNum() == 6)
@@ -15,23 +16,23 @@ def count_P(mol):
 def count_S(mol):
     return sum(1 for atom in mol.GetAtoms() if atom.GetAtomicNum() == 16)
 def count_X(mol):
-    return sum(1 for atom in mol.GetAtoms() if atom.GetAtomicNum() == 9 or atom.GetAtomicNum() == 17 or 
-atom.GetAtomicNum() == 35 or atom.GetAtomicNum() == 53)
+    return sum(1 for atom in mol.GetAtoms() if atom.GetAtomicNum() == 9 or atom.GetAtomicNum() == 17 or
+        atom.GetAtomicNum() == 35 or atom.GetAtomicNum() == 53)
 def count_H(mol):
     H = 0
     for i in range(mol.GetNumAtoms()):
         H += mol.GetAtomWithIdx(i).GetTotalNumHs(includeNeighbors=True)
     return H
 
-from rdkit.Chem.Descriptors import MolWt
-
 def cpd_inform(SMILES):
-    
-    """A function for getting compound information from SMILES string
-    it received a SMILES string and return a dictionary of information consisted of number of C, H, O , N, P, S, 
-X, Degree of Unsaturation and Molecular Weight"""
+    """
+    A function for getting compound information from SMILES string it received
+        a SMILES string and return a dictionary of information consisted of
+        number of C, H, O , N, P, S, X, Degree of Unsaturation
+        and Molecular Weight
+    """
     info = []
-    mol = Chem.MolFromSmiles(SMILES)
+    mol = Chem.rdmolfiles.MolFromSmiles(SMILES)
     info.append(float(count_C(mol)))
     info.append(float(count_H(mol)))
     info.append(float(count_O(mol)))
@@ -45,14 +46,12 @@ X, Degree of Unsaturation and Molecular Weight"""
 
 # Create a function that create a new column of chemical information
 
-import pandas as pd
-
 def create_cpd_info(input_df, col_name='SMILES'):
-    
-    """Receive a DataFrame and return a dataframe with additional columns named n_C, n_H, ..., DoU, and MW"""
-    
-    # create an empty funciton with either empty strint '' or NaN by np.nan
 
+    """
+    Receive a DataFrame and return a dataframe with additional columns
+        named n_C, n_H, ..., DoU, and MW
+    """
     n_C = []
     n_H = []
     n_O = []
@@ -63,8 +62,8 @@ def create_cpd_info(input_df, col_name='SMILES'):
     DoU = []
     MW = []
 
-    for row in range(input_df.shape[0]):
-        mol = input_df[col_name][row]
+    for _, row in input_df.iterrows():
+        mol = row[col_name]
         info = cpd_inform(mol)
         n_C.append(info[0])
         n_H.append(info[1])
@@ -76,14 +75,14 @@ def create_cpd_info(input_df, col_name='SMILES'):
         DoU.append(info[7])
         MW.append(info[8])
 
-    input_df['n_C'] = pd.DataFrame(n_C)
-    input_df['n_H'] = pd.DataFrame(n_H)
-    input_df['n_O'] = pd.DataFrame(n_O)
-    input_df['n_N'] = pd.DataFrame(n_N)
-    input_df['n_P'] = pd.DataFrame(n_P)
-    input_df['n_S'] = pd.DataFrame(n_S)
-    input_df['n_X'] = pd.DataFrame(n_X)
-    input_df['DoU'] = pd.DataFrame(DoU)
-    input_df['MW'] = pd.DataFrame(MW)
-    
+    input_df['n_C'] = n_C
+    input_df['n_H'] = n_H
+    input_df['n_O'] = n_O
+    input_df['n_N'] = n_N
+    input_df['n_P'] = n_P
+    input_df['n_S'] = n_S
+    input_df['n_X'] = n_X
+    input_df['DoU'] = DoU
+    input_df['MW'] = MW
+
     return input_df
