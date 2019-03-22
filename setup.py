6927e72ba6259ec7c4748fa7489aa8e4037a0020
Cholpisit Ice Kiattisewee <cholpk@uw.edu>
2019-03-21 22:10:20 -0700

    update coverage

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'MyPackageName',
+    name = 'metamoles',
     version = '1.0.0',
-    url = 'https://github.com/mypackage.git',
-    author = 'Author Name',
-    author_email = 'author@gmail.com’,
-    description = 'Description of my package',
-    packages = find_packages(),    
+    url = 'https://github.com/theicechol/metamoles.git',
+    author = 'theicechol',
+    author_email = 'cholpisit@gmail.com’,
+    description = 'UWDIRECT-MetaMolES',
+    packages = find_packages(),
     install_requires = ['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
