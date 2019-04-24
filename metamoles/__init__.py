6d0027fca24c98c7cc4d56c241ee94c327b08e75
Dave Beck <dacb@uw.edu>
2019-04-24 09:46:48 -0700

    fixed up testing

--- metamoles/__init__.py
+++ metamoles/__init__.py
@@ -1,1 +1,5 @@
-from . import metamoles
+from .metamoles import create_kegg_df, kegg_df_to_smiles, sid_to_smiles
+from .metamoles import create_cpd_info, fingerprint_products, input_data
+from .metamoles import cpd_inform, select_promiscuous_enzymes
+from .metamoles import parse_compound_ids, create_negative_matches
+from .metamoles import remove_single_cpd_rows, binarize_enzyme_class
